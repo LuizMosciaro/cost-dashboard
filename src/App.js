f78288bde025645972337c25d312b4bb61cd2a8d
luizmosciaro <edu.mosciaro@live.com>
2022-12-04 13:54:41 -0400

    Aula 19 - Navbar e footer + estilizacao

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import Projects from './components/pages/Projects';
 
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-      
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Container customClass='min_height'>
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path='/contact' element={<Contact />}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path='/company' element={<Company />}></Route>
+        </Routes>
+        <Routes>
+            <Route exact path='/projects' element={<Projects />}></Route>
+        </Routes>
+      </Container>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
