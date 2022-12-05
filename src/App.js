e09f1428c8e51fa0f5b28c295dbf73a0ca6b310d
luizmosciaro <edu.mosciaro@live.com>
2022-12-05 17:21:11 -0400

    Aula 20 - home e botao

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import Projects from './components/pages/Projects';
+import NewProject from './components/pages/NewProject';
 
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
@@ -24,6 +25,9 @@ function App() {
         </Routes>
         <Routes>
             <Route exact path='/projects' element={<Projects />}></Route>
+        </Routes>
+        <Routes>
+            <Route exact path='/newproject' element={<NewProject />}></Route>
         </Routes>
       </Container>
       <Footer/>
