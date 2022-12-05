7af383198e9f533c27ad31f1a0d39ac18417837d
luizmosciaro <edu.mosciaro@live.com>
2022-12-05 19:47:29 -0400

    Aula 23 conectando a api e passando opt pro Select

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,3 +1,5 @@
+import {useEffect, useState} from 'react'
+
 import Input from '../form/Input.js'
 import Select from '../form/Select.js'
 import SubmitButton from '../form/SubmitButton.js'
@@ -5,6 +7,37 @@ import SubmitButton from '../form/SubmitButton.js'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}) {
+
+    const [categories,setCategories] = useState([])
+
+    /** OBS: Dessa forma fica num loop infiniito de requests, usar o useEffect 
+     * fetch("http://localhost:5000/categories",{
+        method:'GET',
+        headers:{
+            'Content-Type':'application/json'
+        },    
+    })  
+        .then((response) => response.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err)=> console.log(err)) */
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/categories",{
+        method:'GET',
+        headers:{
+            'Content-Type':'application/json'
+        },    
+    })  
+        .then((response) => response.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err)=> console.log(err))
+    }, []
+    )
+
     return (
         <form className={styles.form}>
             <Input
@@ -17,7 +50,11 @@ function ProjectForm({btnText}) {
                 text='Orcamento'
                 name='budget'
                 placeholder='Insira o nome do projeto' />
-        <Select name='category_id' text='Selecione uma categoria'/>
+        <Select 
+            name='category_id' 
+            text='Selecione uma categoria'
+            options={categories}
+        />
         <SubmitButton text={btnText}/>
         </form>
     )
