4636d1d5ea9cf13c3c1b80b97a427f84c98d731b
luizmosciaro <edu.mosciaro@live.com>
2022-12-07 17:39:45 -0400

    Envio para api (quebrado)

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,33 @@
+
 import styles from './NewProject.module.css'
 import ProjectForm from '../project/ProjectForm'
 
 function NewProject(){
+
+    function createPost(project) {
+        // initialize cost and services
+        project.cost = 0
+        project.services = []
+
+        fetch("https://localholst:5000/projects",{
+            method:'POST',
+            headers:{
+                'Content-type':'application/json',
+            },
+            body: JSON.stringify(project),
+        }).then(
+            resp => resp.json()
+        ).then((data) => console.log(data),
+            //redirect
+            //history('/projects', { message: 'Projeto criado com sucesso!' })
+        ).catch((err) => console.log(err))
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projecto para adicionar os servicos</p>
-            <ProjectForm btnText={'Criar Projeto'}/>
+            <ProjectForm handleSubmit={createPost} btnText={'Criar Projeto'}/>
         </div>
     )
 }
